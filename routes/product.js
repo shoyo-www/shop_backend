856d4c42329ca08154b1a16acd0ca57347fa76aa
Rohit <shoyo.striker@gmail.com>
2024-11-20 19:58:33 +0530

    api

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Product = require('../models/product');
+const mongoose = require('mongoose');
 const verify = require('./verify_token');
 
 const productRouter = express.Router();
@@ -24,6 +25,24 @@ productRouter.get('/api/products',verify,async(req,res)=>{
     res.status(500).json({status : false,msg : error.message});
     console.log(error);
 }
+});
+
+productRouter.get('/api/single/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({ status : false,msg: "Invalid product ID" });
+        }
+        const product = await Product.findById(id); 
+
+        if (!product) {
+            return res.status(404).json({ status : false,msg: "Product not found" });
+        }
+        return res.status(200).json({ status : true,msg: "Fetched successfully", product });
+    } catch (error) {
+        res.status(500).json({ msg: error.message });
+        console.error(error);
+    }
 });
 
 
